e8e9a42e2b060f313e1b86f057313488a10f63a8
Zachary Hamm <zjhamm304@gmail.com>
2024-04-12 12:06:11 -0400

    fix:typings declarations

--- src/types/page.object.ts
+++ src/types/page.object.ts
@@ -1,7 +1,7 @@
 import { IMetadata } from "@src/types/element.collection";
 
 export interface IPageMetadata extends IMetadata {
-    baseUrl: string;
-    path: string | "";
     title?: string;
+    baseUrl?: string; //If not provided, the baseUrl from the Cypress config is used
+    path: string | "";
 }
