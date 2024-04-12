0a167e0b6a2fd8867b6c508d44dd6b92cc9a27a1
Zachary Hamm <zjhamm304@gmail.com>
2024-04-12 12:09:00 -0400

    fix:typings declarations

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import ElementCollection from "./element.collection";
 import ComponentObject from "./component.object";
 import PageObject from "./page.object";
 
-export * from "./types/element.collection";
-export * from "./types/page.object";
+export * from "./types";
 export { ElementCollection, ComponentObject, PageObject };
